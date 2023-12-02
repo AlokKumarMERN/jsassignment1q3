ba4f9413f682d3e1222ce87a7487496fa4bf94ba
AlokKumarMERN <142483957+AlokKumarMERN@users.noreply.github.com>
2023-12-02 16:27:59 +0530

    Add files via upload

---
+++ Question3.js
@@ -0,0 +1,11 @@
+let a = 12;
+let b = 20;
+if(a%10==0 && b%10==0){
+    console.log("true");
+}
+if(a%10!=0 && b%10!=0){
+    console.log("false");
+}
+if(a%10==0 || a%10==0){
+    console.log("true");
+}
